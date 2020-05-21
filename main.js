dbc8b85619c04281c5c0e7b40f42994e3fad627a
thedevyansh <schawla333333@gmail.com>
2020-05-21 16:57:15 +0530

    .

--- main.js
+++ main.js
@@ -3,4 +3,14 @@ $(document).ready(function() {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
     });
+
+    $('.top-nav .nav-link').on('click', function() {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+
+    $('#up').on('click', function() {
+        $('html, body').animate({ scrollTop: 0 }, 100);
+    });
 });
+
