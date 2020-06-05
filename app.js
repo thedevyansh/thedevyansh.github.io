5f580b0aad3de65e2bf07cf1f486e5cca65b9e36
thedevyansh <schawla333333@gmail.com>
2020-06-05 09:41:52 +0530

    .

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-let formMessage = firebase.database().ref('messages');
+var firebaseConfig = {
+  apiKey: "AIzaSyCkEshJgMnos8rwpV2FtprEzUjwA4M_YQQ",
+  authDomain: "portfolio-12648.firebaseapp.com",
+  databaseURL: "https://portfolio-12648.firebaseio.com",
+  projectId: "portfolio-12648",
+  storageBucket: "portfolio-12648.appspot.com",
+  messagingSenderId: "91591628217",
+  appId: "1:91591628217:web:9d0849a461e1ec2fc71268",
+  measurementId: "G-7RHEXR9ZDW"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+let formMessage = firebase.database().ref('messages');
 
 const messageform = document.querySelector('#messageform');
 
